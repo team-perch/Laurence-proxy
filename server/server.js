7a522ee6032b2eda96a4ca602fe80b8247b8511a
Laurence <ldn221@nyu.edu>
2020-01-08 11:20:31 -0800

    Add proxy middleware for routes and stress testing

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 /* eslint-disable no-console */
+require('newrelic');
 const express = require('express');
 const path = require('path');
+const httpMiddleware = require('http-proxy-middleware');
 
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
+
+const imageGalleryOptions = {
+  target: 'http://52.9.82.201:3003',
+  changeOrigin: true,
+};
+
+const imageGalleryProxy = httpMiddleware(imageGalleryOptions);
+
+app.use('/api/images/', imageGalleryProxy);
 
 const port = 3000;
 app.listen(port, console.log(`listening on ${port}`));
